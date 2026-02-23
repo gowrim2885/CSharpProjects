fccda1c662161e80856bd2b53b3dd70282e94639
gowrim2885 <gowrim2885@gmail.com>
2026-02-23 18:18:07 +0530

    ADDED: New files

---
+++ CSharpProjects/DBConnection/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            string connctionString = "Data Source=.;Initial Catalog=EmployeeDatabase;User ID=sa;Password=Gowri@2212;";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connctionString))
+                {
+                    string query = "SELECT * FROM Employee";
+                    using (SqlDataAdapter da = new SqlDataAdapter(query, connection))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();   
+        }
+
+
+
+    }
+}
---
+++ CSharpProjects/Labs/Program.cs
@@ -0,0 +1,20 @@
+﻿using Labs;
+using System;
+//Class_Examples.Main();
+
+//Transaction.Main();
+
+
+//Console.WriteLine("Enter");
+//int b = Console.Read();
+//Console.WriteLine(b);
+
+//Console.WriteLine("Enter");
+//int a = Console.ReadKey();
+//Console.WriteLine(a);
+
+//Asysnchronous.Main();
+
+await SyncMethods.Main();
+
+//RefAndOut.RunMain();
