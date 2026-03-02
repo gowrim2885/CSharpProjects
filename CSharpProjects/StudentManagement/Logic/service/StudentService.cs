112b819cc8ada6a88cf4cd1b5b57da0f3774f223
gowrim2885 <gowrim2885@gmail.com>
2026-03-02 17:44:52 +0530

    ADDED: Student management Console App

---
+++ CSharpProjects/StudentManagement/Logic/service/StudentService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StudentManagementSystem.Logic.Models;
+using StudentManagementSystem.Data;
+
+namespace StudentManagementSystem.Logic.service
+{
+    internal class StudentService
+    {
+        StudentData data = new StudentData();
+        public void AddNewStudent(int nId, string strName, int nAge, string strEmail, string strDepartment)
+        {
+            data.AddStudent(new StudentModel
+            {
+                StudentId = nId,
+                StudentName = strName,
+                Age = nAge,
+                Email = strEmail,
+                Department = strDepartment
+
+            }
+            );
+        }
+    }
+}
---
+++ CSharpProjects/StudentManagement/UI/StudentManagement.cs
@@ -0,0 +1,55 @@
+﻿using StudentManagementSystem.Logic.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.UI
+{
+    internal class StudentManagement
+    {
+        StudentService service = new StudentService();
+        public void Start()
+        {
+
+            while(true)
+            {
+                Console.WriteLine(" *** WELCOME TO STUDENT MANAGEMENT SYSTEM ***");
+
+                Console.WriteLine("Enter Which Operation are to be performed");
+                Console.WriteLine("1. Add Students");
+                Console.WriteLine("2. Display Students");
+
+                int n = Convert.ToInt32(Console.Read());
+
+                switch (n)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                }
+
+            }
+
+        }
+
+        public void AddStudent()
+        {
+            Console.Write("Enter Student ID :");
+            int nId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Student Name :");
+            string? strName = Console.ReadLine();
+            Console.Write("Enter Student Age :");
+            int nAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Email :");
+            string? strEmail = Console.ReadLine();
+            Console.WriteLine("Enter Student Department :");
+            string? strDepartment = Console.ReadLine();
+
+            service.AddNewStudent(nId, strName, nAge, strEmail, strDepartment);
+
+        }
+
+    }
+}
